88a0216811ae7592a6fc75f128a4d6242f512df6
1010varun <varunagarwal10.2@gmail.com>
2023-10-04 10:48:27 +0530

    added session button

--- js/form.js
+++ js/form.js
@@ -16,24 +16,42 @@ var contactFormDB = firebase.database().ref("coderspree");
 document.getElementById("regform").addEventListener("submit", submitForm);
 
 function submitForm(e) {
-  e.preventDefault();
+    e.preventDefault();
 
     const name = getvalue("name");
     const mail = getvalue("mail");
     const library = getvalue("library");
     const github = getvalue("github");
-    savedata(name, mail, library, github);
+    const session = document.getElementsByName("session");
+    var attend = ""
 
-    document.querySelector('.alert').style.display = "block"
+    for (var i = 0; i < session.length; i++) {
+                if (session[i].checked)
+                    attend = session[i].value
+    }
+    
+    if (attend.length === 0) {
+        document.querySelector(".alertDan").style.display = "block";
+        setTimeout(() => {
+            document.querySelector(".alertDan").style.display = "none";
+        }, 2000);
+    }
 
-    setTimeout(() => {
-        document.querySelector(".alert").style.display = "none";
-    }, 2000)
+    else {
 
-    document.getElementById("regform").reset();
+        savedata(name, mail, library, github, attend);
+
+        document.querySelector('.alert').style.display = "block"
+
+        setTimeout(() => {
+            document.querySelector(".alert").style.display = "none";
+        }, 2000)
+
+        document.getElementById("regform").reset();
+    }
 }
 
-const savedata = (name, mail, library, github) => {
+const savedata = (name, mail, library, github, session) => {
     const newForm = contactFormDB.push();
 
     newForm.set({
@@ -41,6 +59,7 @@ const savedata = (name, mail, library, github) => {
         email: mail,
         libraryID: library,
         github: github,
+        attendSession: session,
     })
 }
 
